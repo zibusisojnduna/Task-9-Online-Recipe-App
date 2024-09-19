8c4da5d65a008bdfe81295aaf01de1fd59314a60
User <User@WRSD-LD309-026.dut.ac.za>
2024-09-19 15:56:37 +0200

    feat:layout updated, landing updated

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom"
+
 function Landing(){
     return(
         <section style={{display:"flex"}}>
             <div>
             <h1 style={{color:"white", fontWeight:"bolder", fontSize:"xx-large"}}>Welcome to RECIPEACE,<br></br>if you want to be<br></br>at peace for your<br></br>meal plan, find a recipe.</h1>
                 <div>
-                    <button style={{backgroundColor:"#002500"}}>Login</button><br></br>
+                    <button style={{backgroundColor:"#002500", color:"white", borderRadius:"20%", padding:"5%"}}><Link to={"/Login"}>Login</Link></button><br></br>
                     <h2 style={{color:"white"}}>or if yo don't have an account</h2><br></br>
-                    <button style={{backgroundColor:"#002500"}}>Sign Up</button>
+                    <button style={{backgroundColor:"#002500", color:"white", borderRadius:"20%", padding:"5%"}}><Link to={"/Signup"}>Sign Up</Link></button>
                 </div>          
             </div>
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react"
+
 function Login(){
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [error, setError] = useState()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const response = await fetch("")
+    }
+
+    
     return(
-        <></>
+        <section style={{textAlign:"center"}}>
+            <form>
+                <input type="text" placeholder="Enter Email Address" required></input><br></br>
+                <input type="text" placeholder="Enter Password" required></input><br></br>
+                <button style={{backgroundColor:"#002500"}}>Login</button>
+            </form>
+        </section>
     )
 }
 export default Login
