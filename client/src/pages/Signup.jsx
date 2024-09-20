f35ecc07c8faf7ac34da46f014b7ce28c0f64c4c
User <User@WRSD-LD309-026.dut.ac.za>
2024-09-20 15:04:06 +0200

    feat:axios added, landing, login, sign up updated

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,14 @@
 function SignUp(){
     return(
-        <section>
-            
+        <section style={{alignItems:"center", display:"flex"}}>
+            <div style={{justifyContent:"center", alignItems:"center"}}>
+                <h1 style={{color:"white", fontSize:"xxx-large"}}>Sign Up</h1>
+                <p>Please enter the follwing details.</p>
+                <input type="text" placeholder="Please enter your email address" style={{width:"50%", padding:"2%"}}></input><br></br>
+                <input type="text" placeholder="Please enter your new password" style={{width:"50%", padding:"2%"}}></input><br></br>
+                <input type="text" placeholder="PLease confirm your password" style={{width:"50%", padding:"2%"}}></input>
+
+            </div>
         </section>
     )
 }
